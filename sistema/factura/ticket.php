c121f8c476ce6550aacf5277885d7d3af4b3e3c2
ODRIK22 <136647357+ODRIK22@users.noreply.github.com>
2025-08-11 15:34:41 -0600

    Update ticket.php

--- sistema/factura/ticket.php
+++ sistema/factura/ticket.php
@@ -24,65 +24,51 @@ $total 		= 0;
 			<tr>
 				<td class="logo_factura">
 					<div style="position: relative; left: 20px;">
-						<img src="img/<?php echo $configuracion['foto']; ?>" alt="Logo">
+						<img src="img/<?php echo $configuracion['foto']; ?>" style="width: 100px; alt="Logo; height: auto;">
 					</div>
 
 				</td>
 			</tr>
 			<tr>
-				<td class="info_empresa">
+				<td class="info_empresa" style="text-align: center; vertical-align: top;">
+	<?php
+	if ($result_config > 0) {
+		$iva = $configuracion['iva'];
+		$moned = $configuracion['moneda'];
+	?>
+		<div style="text-align: center;">
+			<!-- Nombre de la empresa -->
+			<h2 style="font-size: 24px; font-weight: bold; margin: 0 0 10px 0;">
+				<?php echo strtoupper($configuracion['nombre']); ?>
+			</h2>
 
-					<?php
+			<!-- Información de contacto -->
+			<p style="margin: 5px 0;">RUC: <?php echo $configuracion['nit']; ?></p>
+			<p style="margin: 5px 0;">Cel: <?php echo $configuracion['telefono']; ?></p>
+			<p style="margin: 5px 0;"><?php echo $configuracion['razon_social']; ?></p>
+		</div>
+	<?php
+		if ($venta['status'] == 1) {
+			$tipo_pago = 'Contado';
+		} elseif ($venta['status'] == 3) {
+			$tipo_pago = 'Crédito';
+		} else {
+			$tipo_pago = 'Anulado';
+		}
+	}
 
-					if ($result_config > 0) {
-						$iva = $configuracion['iva'];
-						$moned = $configuracion['moneda'];
-					?>
-						<div>
-							<div style="height: 0; position: relative; text-align: center;">
-								<span class="h2" style="position: absolute; top: -50px; left: 50%; transform: translateX(-40%); font-size: 24px; font-weight: bold;">
-									<?php echo strtoupper($configuracion['nombre']); ?>
-								</span>
-							</div>
+	if ($tipo_pago == 'Crédito') {
+		date_default_timezone_set("America/Managua");
+		$fecha = date('d-m-Y', strtotime($venta["fecha"]));
+		$fecha_a_vencer = date('d-m-Y', strtotime($fecha . '+ 30 days'));
+		$vence = '<p style="margin-top: 10px;">Vencimiento: ' . $fecha_a_vencer . '</p>';
+	} else {
+		$vence = '';
+	}
 
-							<p style="padding-top: 200px; margin-left: -30px;">
-							<p style="padding-top: 200px; margin-left: -50px;">
-							<p style="padding-top: 200px;">
-								<span style="margin-left: -0px; display: inline-block;">
-									RUC: <?php echo $configuracion['nit']; ?>
-
-								</span><br>
-								<span style="margin-left: -0px; display: inline-block; margin-top: 8px;">
-									Cel: <?php echo $configuracion['telefono']; ?>
-								</span>
-								<span style="margin-left: -225px; display: inline-block; margin-top: -9px;">
-									<p><?php echo $configuracion['razon_social']; ?></p>
-								</span>
-							</p>
-
-
-						</div>
-					<?php
-						if ($venta['status'] == 1) {
-							$tipo_pago = 'Contado';
-						} elseif ($venta['status'] == 3) {
-							$tipo_pago = 'Crédito';
-						} else {
-							$tipo_pago = 'Anulado';
-						}
-					}
-
-					if ($tipo_pago == 'Crédito') {
-						date_default_timezone_set("America/Managua");
-						$fecha = date('d-m-Y', strtotime($venta["fecha"]));
-						$fecha_a_vencer = date('d-m-Y', strtotime($fecha . '+ 30 days'));
-						$vence = '<p>&nbsp;&nbsp;Vencimiento: ' . $fecha_a_vencer . '</p>';
-					} else {
-						$vence = '';
-					}
-
-					?>
-				</td>
+	echo $vence;
+	?>
+</td>
 			</tr>
 			<tr>
 				<td class="">
@@ -103,115 +89,109 @@ $total 		= 0;
 				</td>
 			</tr>
 		</table>
-		<table id="factura_detalle">
-			<thead>
-				<tr>
-					<th colspan="3" class="textleft"> ------------------------------------------------------------------ Descripción</th>
-				</tr>
-				<tr>
-					<th width="">Código</th>
-					<th width="">Cantidad</th>
-					<th class="" width="">Precio</th>
-					<th class="" width="">Total</th>
-				</tr>
-				<tr>
-					<th colspan="3" class="textleft"> ------------------------------------------------------------------ </th>
-				</tr>
-			</thead>
-			<tbody id="detalle_productos">
-				<?php
+		<table id="factura_detalle" width="100%" cellspacing="0" cellpadding="5" border="0">
+	<thead>
+		<tr>
+			<th colspan="4" style="text-align: left; padding-bottom: 5px;">
+				<strong>Descripción de productos</strong>
+			</th>
+		</tr>
+		<tr>
+			<th style="text-align: left;">Código</th>
+			<th style="text-align: center;">Cantidad</th>
+			<th style="text-align: right;">Precio</th>
+			<th style="text-align: right;">Total</th>
+		</tr>
+		<tr>
+			<td colspan="4"><hr></td>
+		</tr>
+	</thead>
+	<tbody id="detalle_productos">
+		<?php
+		if ($result_detalle > 0) {
+			while ($row = mysqli_fetch_assoc($query_productos)) {
+				$precio_venta = number_format($row['precio_venta'], 2);
+				$precio_total = number_format($row['precio_total'], 2);
+		?>
+			<tr>
+				<td style="text-align: left;"><?php echo $row['codigo']; ?></td>
+				<td style="text-align: center;"><?php echo $row['cantidad']; ?></td>
+				<td style="text-align: right;"><?php echo $precio_venta; ?></td>
+				<td style="text-align: right;"><?php echo $precio_total; ?></td>
+			</tr>
+			<tr>
+				<td colspan="4" style="text-align: left; font-size: 12px; padding-left: 5px;">
+					<?php echo $row['descripcion']; ?>
+				</td>
+			</tr>
+		<?php
+				$subtotal = round($subtotal + $row['precio_total'], 2);
+			}
+		}
 
-				if ($result_detalle > 0) {
+		$impuesto 	= round($subtotal * ($iva / 100), 2);
+		$tl_sniva 	= round($subtotal - $impuesto, 2);
+		$total 		= $tl_sniva + $impuesto;
+		$tl_sniva1  = number_format($tl_sniva, 2);
+		$impuesto1  = number_format($impuesto, 2);
+		$descuento  = number_format($venta['descuento'], 2);
+		?>
+	</tbody>
+	<tfoot id="detalle_totales">
+		<tr><td colspan="4"><hr></td></tr>
 
-					while ($row = mysqli_fetch_assoc($query_productos)) {
-						$precio_venta = number_format($row['precio_venta'], 2);
-						$precio_total = number_format($row['precio_total'], 2);
-				?>
-						<tr>
-							<td colspan="3" width=""><?php echo $row['descripcion']; ?></td>
-						</tr>
-						<tr>
-							<td width=""><?php echo $row['codigo']; ?></td>
-							<td width=""><?php echo $row['cantidad']; ?></td>
-							<td width=""><?php echo $precio_venta; ?></td>
-							<td class=""><?php echo $precio_total; ?></td>
-						</tr>
-				<?php
-						$precio_total = $row['precio_total'];
-						$subtotal = round($subtotal + $precio_total, 2);
-					}
-				}
+		<tr>
+			<td colspan="2"></td>
+			<td style="text-align: right;"><strong>Subtotal:</strong></td>
+			<td style="text-align: right;"><?php echo $moned . ' ' . number_format($subtotal, 2); ?></td>
+		</tr>
+		<tr>
+			<td colspan="2"></td>
+			<td style="text-align: right;"><strong>Descuento:</strong></td>
+			<td style="text-align: right;"><?php echo $moned . ' ' . $descuento; ?></td>
+		</tr>
+		<tr>
+			<td colspan="2"></td>
+			<td style="text-align: right;"><strong>TOTAL:</strong></td>
+			<td style="text-align: right;"><?php echo $moned . ' ' . number_format($total - $venta['descuento'], 2); ?></td>
+		</tr>
+	</tfoot>
+</table>
 
-				$impuesto 	= round($subtotal * ($iva / 100), 2);
-				$tl_sniva 	= round($subtotal - $impuesto, 2);
-				$total 		= $tl_sniva + $impuesto;
-				$tl_sniva1 = number_format($subtotal - $impuesto, 2);
-				$impuesto1 = number_format($subtotal * ($iva / 100), 2);
-				$descuento = number_format($venta['descuento'], 2);
-				?>
-			</tbody>
-			<tfoot id="detalle_totales">
-				<tr>
-					<td colspan="3">
-						<p>---------------------------------------------------------</p>
-					</td>
-				</tr>
-				<tr>
-					<td></td>
-					<td></td>
-					<td colspan="" class=""><span>Sub total </span></td>
-					<td class=""><span> <?php echo $moned . ' ' . number_format($total, 2); ?></span></td>
-				</tr>
-				<tr>
-					<td></td>
-					<td></td>
-					<td colspan="" class=""><span>Descuento </span></td>
-					<td class=""><span> <?php echo $moned . ' ' . $descuento; ?></span></td>
-				</tr>
-				<tr>
-					<td></td>
-					<td></td>
-					<td colspan="" class=""><span>TOTAL </span></td>
-					<td class=""><span> <?php echo $moned . ' ' . number_format($total - $descuento, 2); ?></span></td>
-				</tr>
-			</tfoot>
-		</table>
 		<br>
 		<br>
 		<br>
 
-		<div style="text-align: center; margin-top: 10px;"> <!-- Reducir el margen superior al mínimo -->
-			<!-- Título del código de barras -->
-			<p style="font-size: 18px; font-weight: bold; margin: 0 0 5px 0; position: relative; top: -60px;">Código de barras</p> <!-- Subir el título con "top: -10px;" -->
 
-			<div style="display: inline-block; padding: -30px; border: 1px solid #ddd; border-radius: 5px; background-color: #fff; margin-bottom: 10px; position: relative; left: 50px;"> <!-- Mover todo el contenedor hacia la derecha -->
-				<div style="position: relative; top: -10px;"> <!-- Subir solo el gráfico -->
-					<?php
-					require_once __DIR__ . '/../../vendor/autoload.php'; // Ajusta la ruta si es necesario
-					use Picqer\Barcode\BarcodeGeneratorHTML;
+<!-- Contenedor del código de barras -->
+<div style="text-align: center; margin-top: 5px;">
+    <div style="
+        display: inline-block;
+        padding: 25	px;
+        border: 0px solid #ddd;
+        border-radius: 0px;
+        background-color: #fff;
+    ">
+        <?php
+        require_once __DIR__ . '/../../vendor/autoload.php';
+        use Picqer\Barcode\BarcodeGeneratorPNG;
 
-					$generator = new BarcodeGeneratorHTML();
-					$codigo_barra = isset($venta['noventa']) ? $venta['noventa'] : '000000000'; // Valor del código
-					echo $generator->getBarcode($codigo_barra, $generator::TYPE_CODE_128);
-					?>
-				</div>
-			</div>
+        $generator = new BarcodeGeneratorPNG();
+        $codigo_barra = isset($venta['noventa']) ? $venta['noventa'] : '000000000';
+        $barcode_image = base64_encode($generator->getBarcode($codigo_barra, $generator::TYPE_CODE_128));
+        echo '<img src="data:image/png;base64,' . $barcode_image . '" alt="Código de barras">';
+        ?>
+    </div>
+
+    <!-- Texto del número del código de barras -->
+    <p style="margin-top: 100px; font-size: 16px; font-weight: bold;">
+        Número: <?php echo $codigo_barra; ?>
+    </p>
+</div>
 
 
 
-			<!-- Texto del número de barra -->
-			<p style="margin-top: 5px; font-size: 16px; font-weight: bold;">Número: <?php echo $codigo_barra; ?></p> <!-- Reducir margen superior del número -->
-		</div>
-
-
-
-		<div>
-			<h4 class="label_gracias">¡Gracias por su compra!</h4>
-			<p class="nota_devolucion">Revise su producto, no aceptamos devoluciones.</p>
-		</div>
-
-	</div>
 
 </body>
 
-</html>
+</html>s
